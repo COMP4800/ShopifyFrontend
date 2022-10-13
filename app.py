e2429607a52777f3b237a871991560a8fb0d0eb7
rosudoria <77439184+rosudoria@users.noreply.github.com>
2022-10-12 22:00:57 -0700

    update routes with pseudo DB

--- app.py
+++ app.py
@@ -1,6 +1,62 @@
 from flask import Flask
+import re
 
 app = Flask(__name__)
+
+# Pseudo DB
+DB = {
+    "clients": [
+        {"_id": 1}, {"_id": 2}, {"_id": 3}
+    ],
+    "orders": [
+        {
+            "_id": 1,
+            "client_id": 1,
+            "customer_id": 1,
+            "month": "2/31/2021",
+            "orders": 1,
+            "gross_sales": 497,
+            "discounts": -497,
+            "returns": 0,
+            "net_sales": 0,
+            "shipping": 0,
+            "taxes": 0,
+            "total_sales": 0,
+            "average_order_value": 0
+        },
+        {
+            "_id": 2,
+            "client_id": 2,
+            "customer_id": 18,
+            "month": "7/31/2022",
+            "orders": 1,
+            "gross_sales": 497,
+            "discounts": -497,
+            "returns": 0,
+            "net_sales": 0,
+            "shipping": 0,
+            "taxes": 0,
+            "total_sales": 0,
+            "average_order_value": 0
+        },
+        {
+            "_id": 3,
+            "client_id": 1,
+            "customer_id": 17,
+            "month": "7/31/2022",
+            "orders": 1,
+            "gross_sales": 51,
+            "discounts": -7.65,
+            "returns": 0,
+            "net_sales": 43.35,
+            "shipping": 9.99,
+            "taxes": 0,
+            "total_sales": 53.34,
+            "average_order_value": 53.34
+        },
+
+    ],
+}
 
 
 # Place holder index
@@ -13,24 +69,38 @@ def initialize_frontend():
 @app.route('/orders/<client_id>')
 def get_client_orders(client_id):
     # Query database for result
+    data = [_each for _each in DB["orders"] if _each["client_id"] == int(f"{client_id}")]
     # Return result of query in Data
-    return {"client_id": client_id, "data": "REPLACE ME"}
+    return {"client_id": client_id, "data": data}
 
 
 # Get all orders from a specified year of Client ID to date
 @app.route('/orders/<client_id>/<year>')
 def get_client_orders_year(client_id, year):
     # Query database for result
+    data = []
+    for _each in DB["orders"]:
+        if _each["client_id"] == int(f"{client_id}"):
+            date = _each["month"]
+            if re.search(f"/{year}", f"{date}"):
+                data.append(_each)
     # Return result of query in Data
     return {"client_id": client_id,
             "year": year,
-            "data": "REPLACE ME"}
+            "data": data}
 
 
 # Get all orders from a specified year and month of Client ID to date
 @app.route('/orders/<client_id>/<year>/<month>')
-def retrieve_client_orders_year_month(client_id, year, month):
+def get_client_orders_year_month(client_id, year, month):
     # Query database for result
+    data = []
+    for _each in DB["orders"]:
+        if _each["client_id"] == int(f"{client_id}"):
+            date = _each["month"]
+            if re.search(f"^{month}./{year}", f"{date}"):
+                data.append(_each)
+
     # Return result of query in Data
     return {"client_id": client_id,
             "year": year,
--- test.py
+++ test.py
@@ -5,11 +5,11 @@ import requests
 # Open a second terminal and run test.py to tests response from app.py routes
 BASE_URL = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE_URL + "orders/12345")
+response = requests.get(BASE_URL + "orders/1")
+# print(response.json())
+
+response = requests.get(BASE_URL + "orders/1/2022")
 print(response.json())
 
-response = requests.get(BASE_URL + "orders/12345/2020")
-print(response.json())
-
-response = requests.get(BASE_URL + "orders/12345/2020/01")
-print(response.json())
+response = requests.get(BASE_URL + "orders/1/2020/01")
+# print(response.json())
