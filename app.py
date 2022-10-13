058b1d163a994b488b0d4376a356a15f8e791d56
rosudoria <77439184+rosudoria@users.noreply.github.com>
2022-10-12 17:14:14 -0700

    Add basic routes without queries

--- app.py
+++ app.py
@@ -3,6 +3,36 @@ from flask import Flask
 app = Flask(__name__)
 
 
+# Place holder index
 @app.route('/')
 def initialize_frontend():
-    return 'Welcome to the start of the Frontend For COMP 4800'
+    return {"data": 'Hello world!'}
+
+
+# Get all orders from Client ID to date
+@app.route('/orders/<client_id>')
+def get_client_orders(client_id):
+    # Query database for result
+    # Return result of query in Data
+    return {"client_id": client_id, "data": "REPLACE ME"}
+
+
+# Get all orders from a specified year of Client ID to date
+@app.route('/orders/<client_id>/<year>')
+def get_client_orders_year(client_id, year):
+    # Query database for result
+    # Return result of query in Data
+    return {"client_id": client_id,
+            "year": year,
+            "data": "REPLACE ME"}
+
+
+# Get all orders from a specified year and month of Client ID to date
+@app.route('/orders/<client_id>/<year>/<month>')
+def retrieve_client_orders_year_month(client_id, year, month):
+    # Query database for result
+    # Return result of query in Data
+    return {"client_id": client_id,
+            "year": year,
+            "month": month,
+            "data": "REPLACE ME"}
