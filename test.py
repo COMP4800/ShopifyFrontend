1e11857f8147ff2807e7526926f9a6d6a04a8474
JashanpreetSingh-dev <77453449+JashanpreetSingh-dev@users.noreply.github.com>
2022-10-13 18:34:49 -0700

    Changed the name of app.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import requests
 
 # Testing URL
 # Open a terminal and run wsgi.py
-# Open a second terminal and run test.py to tests response from app.py routes
+# Open a second terminal and run test.py to tests response from main.py routes
 BASE_URL = "http://127.0.0.1:5000/"
 
 response = requests.get(BASE_URL + "orders/1")
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from app import app
+from main import app
 
 # Do not add any code here
 if __name__ == '__main__':
