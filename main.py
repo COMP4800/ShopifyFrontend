9080af474858f22bd333b4d704d83f3a59f8206d
JashanpreetSingh-dev <77453449+JashanpreetSingh-dev@users.noreply.github.com>
2022-10-28 01:51:37 -0700

    Add the route to get orders by month

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ import boto3
 import os
 from boto3.dynamodb.conditions import Key
 
-
 app = Flask(__name__)
 
 # MONGO_CONNECTION_URI = "mongodb+srv://adminTester:ETh5oidcvfuVCwWr@testinggrounds.brdchna.mongodb.net/?retryWrites
@@ -28,6 +27,26 @@ dynamodb_client = boto3.client(service_name='dynamodb',
                                region_name="ca-central-1")
 
 
+def dump_table(table_name):
+    results = []
+    last_evaluated_key = None
+    while True:
+        if last_evaluated_key:
+            response = dynamodb_client.scan(
+                TableName=table_name,
+                ExclusiveStartKey=last_evaluated_key
+            )
+        else:
+            response = dynamodb_client.scan(TableName=table_name)
+        last_evaluated_key = response.get('LastEvaluatedKey')
+
+        results.extend(response['Items'])
+
+        if not last_evaluated_key:
+            break
+    return results
+
+
 def get_items_from_db(table_name):
     """
     Get all the orders from AWS DynamoDb
@@ -36,12 +55,23 @@ def get_items_from_db(table_name):
     """
     try:
         table = dynamodb.Table(table_name)
-        response = table.scan()['Items']
-        print(response)
-    except ClientError as e:
-        print(f"error: {e}")
-    else:
-        return response
+        response = table.scan()
+        data = response['Items']
+        while 'LastEvaluatedKey' in response:
+            response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+            data.extend(response['Items'])
+        return data
+    except ClientError as err:
+        return err
+
+    # try:
+    #     response = table.scan()['Items']
+    #     print(response)
+    # except ClientError as e:
+    #     print(f"error: {e}")
+    # else:
+    #     return response
+
 
 # Pseudo DB
 # Db = {
@@ -99,13 +129,20 @@ def get_items_from_db(table_name):
 # }
 
 
-# Place holder index
-@app.route('/')
+@app.route("/")
+def welcome():
+    return {"Message": "welcome to the EightX API"}
+
+
+@app.route('/orders')
 def initialize_frontend():
     """
     Route for getting all the orders
     """
+    # return dump_table("keep-it-wild-az")
     return get_items_from_db("keep-it-wild-az")
+
+
 #
 #
 # # Get all orders from Client ID to date
@@ -182,6 +219,69 @@ def initialize_frontend():
 #     return {"client_id": client_id, "customer_id": customer_id, "first_order_date": first_date}
 
 
+@app.route('/orders/<year>/<month>')
+def get_monthly_orders(year, month):
+    """
+    This route gets all the orders by month for a single year
+    :param year: a number
+    :param month: a number
+    :return: a JSON
+    """
+    # table_name = "keep-it-wild-az"
+    # results = []
+    # last_evaluated_key = None
+    # parsed_data = []
+    # while True:
+    #     if last_evaluated_key:
+    #         response = dynamodb_client.scan(
+    #             TableName=table_name,
+    #             ExclusiveStartKey=last_evaluated_key
+    #         )
+    #     else:
+    #         response = dynamodb_client.scan(TableName=table_name)
+    #     last_evaluated_key = response.get('LastEvaluatedKey')
+    #
+    #     results.extend(response['Items'])
+    #     print(len(results))
+    #
+    #     for each_item in response['Items']:
+    #         # print(each_item["OrderDate"])
+    #         if len(each_item["OrderDate"]) == 20:
+    #             date = datetime.datetime.fromisoformat(each_item["OrderDate"].rstrip(each_item["OrderDate"][-1]))
+    #             if date.year == int(year) and date.month == int(month):
+    #                 parsed_data.append(each_item)
+    #         else:
+    #             print("no")
+    #
+    #     if not last_evaluated_key:
+    #         break
+    # return parsed_data
+
+
+    try:
+        table = dynamodb.Table("test1")
+        response = table.scan(
+            # FilterExpression=filter_expression
+        )
+        data = response['Items']
+        parsed_data = []
+        while 'LastEvaluatedKey' in response:
+            response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+            data.extend(response['Items'])
+            print(len(data))
+            for each_item in response['Items']:
+                date_year = str(each_item["OrderDate"])[0:4]
+                date_month = str(each_item["OrderDate"])[5:7]
+                if date_year == year and date_month == month:
+                # date = datetime.datetime.fromisoformat(each_item["OrderDate"].rstrip(each_item["OrderDate"][-1]))
+                # if date.year == int(year) and date.month == int(month):
+                    parsed_data.append(each_item)
+
+        return parsed_data
+    except ClientError as err:
+        return {"Error": f"{err}"}
+
+
 @app.route('/orders/<year>')
 def get_yearly_orders(year):
     """
@@ -189,29 +289,57 @@ def get_yearly_orders(year):
     :param year: an int
     :return: a JSON
     """
-    table = dynamodb.Table("keep-it-wild-az")
-    filter_expression = Key('OrderDate').between(f'{year}-01-01', f'{year}-12-31')
-    response = table.scan(
-        FilterExpression=filter_expression
-    )
-    return response['Items']
+
+    try:
+        filter_expression = Key('OrderDate').between(f'{year}-01-01T00:00:00Z', f'{year}-12-31T24:00:00Z')
+        table = dynamodb.Table("keep-it-wild-az")
+        response = table.scan(
+            # FilterExpression=filter_expression
+        )
+        data = response['Items']
+        parsed_data = []
+        while 'LastEvaluatedKey' in response:
+            response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+            data.extend(response['Items'])
+            print(len(data))
+            for each_item in response['Items']:
+                date = datetime.datetime.fromisoformat(each_item["OrderDate"].rstrip(each_item["OrderDate"][-1]))
+                if date.year == int(year):
+                    parsed_data.append(each_item)
+        print(len(parsed_data))
+        return parsed_data
+    except ClientError as err:
+        return err
+
+    # response = table.scan(
+    # )
+    # return response['Items']
 
 
 # @app.route('/orders/<start_date>/<end_date>')
 # def get_order_between_date_range(start_date, end_date):
-#     table = dynamodb.Table("keep-it-wild-az")
-#     filter_expression = Key('OrderDate').between(start_date, f'{end_date}T24:00:00Z')
-#     response = table.scan(
-#         FilterExpression=filter_expression
-#     )
-#     # response = table.query(
-#     #     IndexName="OrderID-OrderDate-index",
-#     #     # KeyConditionExpression=Key('OrderDate').between(f'{start_date}T12:00:00Z', f'{end_date}T12:00:00Z'),
-#     #     KeyCondition=f"OrderID = :id and OrderDate = :{start_date} BETWEEN {end_date}"
-#     #
-#     #     # FilterExpression=f'OrderDate = :OrderDate between :{start_date} and :{end_date}'
-#     #
-#     #
-#     #     # KeyConditionExpression=Key('OrderDate').lt(start_date)
-#     # )
-#     return response['Items']
+#     try:
+#         filter_expression = Key('OrderDate').between(f'{start_date}T00:00:00Z', f'{end_date}T24:00:00Z')
+#         table = dynamodb.Table("keep-it-wild-az")
+#         response = table.scan(
+#             FilterExpression=filter_expression
+#         )
+#         data = response['Items']
+#         while 'LastEvaluatedKey' in response:
+#             response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+#             data.extend(response['Items'])
+#         return data
+#     except ClientError as err:
+#         return err
+
+    # response = table.query(
+    #     IndexName="OrderID-OrderDate-index",
+    #     # KeyConditionExpression=Key('OrderDate').between(f'{start_date}T12:00:00Z', f'{end_date}T12:00:00Z'),
+    #     KeyCondition=f"OrderID = :id and OrderDate = :{start_date} BETWEEN {end_date}"
+    #
+    #     # FilterExpression=f'OrderDate = :OrderDate between :{start_date} and :{end_date}'
+    #
+    #
+    #     # KeyConditionExpression=Key('OrderDate').lt(start_date)
+    # )
+    # return response['Items']
